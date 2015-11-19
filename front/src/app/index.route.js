664bafae8b5d3b73533f40035313f329ae16db77
Mayu Kojima <aquariuslake@gmail.com>
2015-11-19 10:05:15 +0900

    connect to login page

--- front/src/app/index.route.js
+++ front/src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/login/login.html',
+        controller: 'LoginController',
+        controllerAs: 'login'
       });
 
     $urlRouterProvider.otherwise('/');
