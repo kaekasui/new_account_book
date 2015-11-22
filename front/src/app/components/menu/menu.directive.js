28654296623e2a70e9da24526e11549758b0d521
Mayu Kojima <aquariuslake@gmail.com>
2015-11-23 04:31:42 +0900

    remove files

--- front/src/app/components/menu/menu.directive.js
+++ front/src/app/components/menu/menu.directive.js
@@ -6,16 +6,6 @@
     .directive('menuDirective', menuDirective);
 
   function menuDirective() {
-/*
-    var directive = {
-      restrict: 'E',
-      templateUrl: 'app/components/menu/menu.html',
-      scope: {
-          creationDate: '='
-      },
-      controller: MenuController
-    };
-*/
     var directive = {
       restrict: 'C',
       templateUrl: 'app/components/menu/menu.html'
