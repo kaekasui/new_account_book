5d58a82c48ca93858c373c4354682ae4748c6e33
Mayu Kojima <aquariuslake@gmail.com>
2015-12-03 17:16:57 +0900

    set the menu selected icon

--- front/src/app/components/menu/menu.directive.js
+++ front/src/app/components/menu/menu.directive.js
@@ -16,12 +16,24 @@
     return directive;
 
     function MenuController($translate) {
-      this.menus = [
+      this.menu_names = [
         { name: $translate.instant('TITLES.LOGIN'), icon: 'glyphicon-leaf', url: 'login' },
         { name: $translate.instant('TITLES.SIGN_UP'), icon: 'glyphicon-heart', url: 'sign_up' },
         { name: $translate.instant('TITLES.RESET_PASSWORD'), icon: 'glyphicon-leaf', url: 'reset_password' },
         { name: $translate.instant('TITLES.RESEND_EMAIL'), icon: 'glyphicon-heart', url: 'resend_email' }
       ];
+      this.menu_image = 'assets/images/pig_footprints.gif';
+      this.isSelected = function(index) {
+        return this.selected == index;
+      };
+ 
+      // Event
+      this.mouseEnter = function(index) {
+        this.selected = index;
+      };
+      this.mouseLeave = function(index) {
+        this.selected = undefined;
+      };
     }
   }
 })();
