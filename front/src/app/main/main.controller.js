b4349ed46d4477c731bf6435f675a7f11d51698c
Mayu Kojima <aquariuslake@gmail.com>
2015-12-04 10:32:34 +0900

    back to the main.controller.js

--- front/src/app/main/main.controller.js
+++ front/src/app/main/main.controller.js
@@ -1,33 +1,39 @@
 (function() {
-  (function() {
-    'use strict';
-    var MainController;
-    MainController = function($timeout, webDevTec, toastr) {
-      var activate, getWebDevTec, showToastr, vm;
-      vm = this;
-      activate = function() {
-        getWebDevTec();
-        $timeout((function() {
-          vm.classAnimation = 'rubberBand';
-        }), 4000);
-      };
-      showToastr = function() {
-        toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-        vm.classAnimation = '';
-      };
-      getWebDevTec = function() {
-        vm.awesomeThings = webDevTec.getTec();
-        angular.forEach(vm.awesomeThings, function(awesomeThing) {
-          awesomeThing.rank = Math.random();
-        });
-      };
-      vm.awesomeThings = [];
-      vm.classAnimation = '';
-      vm.creationDate = 1447644390420;
-      vm.showToastr = showToastr;
-      activate();
-    };
-    return angular.module('accountBook').controller('MainController', MainController);
-  })();
+  'use strict';
 
-}).call(this);
+  angular
+    .module('accountBook')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout, webDevTec, toastr) {
+    var vm = this;
+
+    vm.awesomeThings = [];
+    vm.classAnimation = '';
+    vm.creationDate = 1447644390420;
+    vm.showToastr = showToastr;
+
+    activate();
+
+    function activate() {
+      getWebDevTec();
+      $timeout(function() {
+        vm.classAnimation = 'rubberBand';
+      }, 4000);
+    }
+
+    function showToastr() {
+      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+      vm.classAnimation = '';
+    }
+
+    function getWebDevTec() {
+      vm.awesomeThings = webDevTec.getTec();
+
+      angular.forEach(vm.awesomeThings, function(awesomeThing) {
+        awesomeThing.rank = Math.random();
+      });
+    }
+  }
+})();
