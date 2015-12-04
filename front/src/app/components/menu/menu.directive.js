9308de1678bfa307fd435ff873f560fe768191af
Mayu Kojima <aquariuslake@gmail.com>
2015-12-04 14:27:09 +0900

    gulp build and once remove

--- front/src/app/components/menu/menu.directive.js
+++ front/src/app/components/menu/menu.directive.js
@@ -15,13 +15,15 @@
 
     return directive;
 
-    function MenuController($translate) {
+    function MenuController() {
+/*
       this.menu_names = [
         { name: $translate.instant('TITLES.LOGIN'), icon: 'glyphicon-leaf', url: 'login' },
         { name: $translate.instant('TITLES.SIGN_UP'), icon: 'glyphicon-heart', url: 'sign_up' },
         { name: $translate.instant('TITLES.RESET_PASSWORD'), icon: 'glyphicon-leaf', url: 'reset_password' },
         { name: $translate.instant('TITLES.RESEND_EMAIL'), icon: 'glyphicon-heart', url: 'resend_email' }
       ];
+*/
       this.menu_image = 'assets/images/pig_footprints.gif';
       this.isSelected = function(index) {
         return this.selected == index;
