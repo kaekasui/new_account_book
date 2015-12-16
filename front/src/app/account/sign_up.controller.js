e9781e8290a7e1aa6f260bb0779c465ecbddfa76
Mayu Kojima <aquariuslake@gmail.com>
2015-12-16 14:09:25 +0900

    add the button function to sign up

--- front/src/app/account/sign_up.controller.js
+++ front/src/app/account/sign_up.controller.js
@@ -5,10 +5,22 @@
       .module('accountBook')
       .controller('SignUpController', SignUpController);
 
-    function SignUpController(menuService) {
+    function SignUpController($http, menuService) {
       var vm = this;
 
       vm.menus = menuService.getMenu();
-    }
-
+      vm.submit = function() {
+        var params = {
+          email: 'email@a.com',
+          password: 'password'
+        };
+        $http.post('/email_user/registrations', params)
+          .success(function(data, status, headers, config) {
+            vm.title = status;
+          })
+          .error(function(data, status, headers, config) {
+            vm.title = status;
+          })
+        }
+      }
 })();
