ee33b87282bee1bc5535810d3c4fbccdc18eeea9
kae_kasui <kaekasui_github@icloud.com>
2015-12-21 10:25:45 +0900

    login and logout, but have to reload page

--- front/src/app/components/navbar/navbar.directive.js
+++ front/src/app/components/navbar/navbar.directive.js
@@ -5,7 +5,7 @@
     .module('accountBook')
     .directive('acmeNavbar', acmeNavbar);
 
-  function acmeNavbar() {
+  function acmeNavbar(IndexFactory, localStorageService, $location) {
     var directive = {
       restrict: 'E',
       templateUrl: 'app/components/navbar/navbar.html',
@@ -19,7 +19,15 @@
 
     return directive;
 
-    function NavbarController() {
+    function NavbarController(IndexFactory, localStorageService, $location) {
+      var vm = this;
+      vm.current_user = IndexFactory.currentUser();
+      //vm.$watch('current_user', function() {
+      //});
+      vm.logout = function() {
+        localStorageService.remove('access_token');
+        $location.path('/');
+      };
     }
   }
 
