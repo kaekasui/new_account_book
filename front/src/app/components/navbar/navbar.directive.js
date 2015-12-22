ab3d5b00911208f6e3f849b03029c768e57e816e
kae_kasui <kaekasui_github@icloud.com>
2015-12-22 14:20:36 +0900

    fixed the errors because controller is out

--- front/src/app/components/navbar/navbar.directive.js
+++ front/src/app/components/navbar/navbar.directive.js
@@ -3,37 +3,39 @@
 
   angular
     .module('accountBook')
+    .controller('NavbarController', NavbarController)
     .directive('acmeNavbar', acmeNavbar);
 
-  function acmeNavbar(IndexFactory, localStorageService, $location) {
+  function acmeNavbar() {
     var directive = {
       restrict: 'E',
       templateUrl: 'app/components/navbar/navbar.html',
+      require: '^acmeNavbar',
       scope: {
           creationDate: '='
       },
-      controller: NavbarController,
+      controller: 'NavbarController',
       controllerAs: 'vm',
       bindToController: true
     };
 
     return directive;
+  }
 
-    function NavbarController(IndexFactory, localStorageService, $location, $scope, toastr, $translate) {
-      var vm = this;
-      $scope.$watch(function() {
-        return $location.path();
-      }, function() {
-        vm.current_user = IndexFactory.currentUser();
-      });
+  function NavbarController(IndexFactory, localStorageService, $location, $scope, toastr, $translate) {
+    var vm = this;
+    $scope.$watch(function() {
+      return $location.path();
+    }, function() {
       vm.current_user = IndexFactory.currentUser();
-      vm.logout = function() {
-        localStorageService.remove('access_token');
-        vm.current_user = IndexFactory.currentUser();
-        toastr.success($translate.instant('MESSAGES.LOGOUT'));
-        $location.path('/');
-      };
-    }
+    });
+    vm.current_user = IndexFactory.currentUser();
+    vm.logout = function() {
+      localStorageService.remove('access_token');
+      vm.current_user = IndexFactory.currentUser();
+      toastr.success($translate.instant('MESSAGES.LOGOUT'));
+      $location.path('/');
+    };
   }
 
 })();
