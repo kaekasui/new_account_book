c1f7bb10ffd6013c71f8ffaa87836ca6bac399cd
kae_kasui <kaekasui_github@icloud.com>
2016-01-05 17:05:30 +0900

    validation to submit

--- front/src/app/account/reset_password.controller.js
+++ front/src/app/account/reset_password.controller.js
@@ -6,6 +6,17 @@
       .controller('ResetPasswordController', ResetPasswordController);
 
     function ResetPasswordController() {
+      var vm = this;
+
+      vm.submit = function() {
+        var params = {
+          email: vm.email
+        };
+
+    //    IndexFactory.postSession(params).catch(function(res) {
+    //      vm.errors = res.error_messages;
+    //    });
+      }
     }
 
 })();
