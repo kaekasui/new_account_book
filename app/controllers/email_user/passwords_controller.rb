54445aebbf8aa864d1c41df2debef3d8cab64bfa
kae_kasui <kaekasui_github@icloud.com>
2016-01-08 11:16:07 +0900

    add the validation to reset password

--- app/controllers/email_user/passwords_controller.rb
+++ app/controllers/email_user/passwords_controller.rb
@@ -4,10 +4,10 @@ class EmailUser::PasswordsController < ApplicationController
   def send_mail
     origin = "#{request.protocol}#{request.host_with_port}"
     email = params[:email]
+    return render_error EmailUser.new, 422 if email.blank?
     @user = EmailUser.find_by(email: email)
     if @user
-      UserMailer.password_reset(email, @user.password_reset_url(origin))
-        .deliver_later
+      @user.reset_password(origin)
     else
       UserMailer.confirmation(email).deliver_later
     end
