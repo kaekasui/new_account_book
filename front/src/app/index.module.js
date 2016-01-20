3bdfd5ddd71ff8665918d9ca9a2da8aacda736cf
kae_kasui <kaekasui_github@icloud.com>
2016-01-20 23:02:07 +0900

    drag the panel

--- front/src/app/index.module.js
+++ front/src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('accountBook', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'pascalprecht.translate', 'LocalStorageModule']);
+    .module('accountBook', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngDragDrop', 'ui.router', 'ui.bootstrap', 'toastr', 'pascalprecht.translate', 'LocalStorageModule']);
 
 })();
