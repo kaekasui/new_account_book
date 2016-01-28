6c7966299cedb61588ba755c957934c2c300c2af
mkojima <kaekasui_github@icloud.com>
2016-01-28 13:24:52 +0900

    Revert "gulpのディレクトリをconfigからrootディレクトリに移動"

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,10 @@ var wrench = require('wrench');
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
  */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+wrench.readdirSyncRecursive('./config/gulp').filter(function(file) {
   return (/\.(js|coffee)$/i).test(file);
 }).map(function(file) {
-  require('./gulp/' + file);
+  require('./config/gulp/' + file);
 });
 
 
