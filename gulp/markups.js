c808a40eb9be0efa3a629044e2c89051893cbb2f
kae_kasui <kaekasui_github@icloud.com>
2016-02-02 18:44:04 +0900

    remove the rename function

--- gulp/markups.js
+++ gulp/markups.js
@@ -10,15 +10,10 @@ var browserSync = require('browser-sync');
 
 var $ = require('gulp-load-plugins')();
 
-gulp.task('markups', ['clean'], function() {
-  function renameToHtml(path) {
-    path.extname = '.html';
-  }
-
+gulp.task('markups', function() {
   return gulp.src(path.join(conf.paths.src, '/app/**/*.haml'))
     .pipe(cache('linting'))
     .pipe(haml())
-    .pipe($.rename(renameToHtml))
     .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app/')))
     .pipe(browserSync.stream());
 });
