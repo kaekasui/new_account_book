adc196ba3418cb0ad3b04b72ab27f7e2e2baa596
kae_kasui <kaekasui_github@icloud.com>
2016-02-02 18:09:36 +0900

    add the clean when build

--- gulp/build.js
+++ gulp/build.js
@@ -95,4 +95,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['clean', 'html', 'fonts', 'other']);
