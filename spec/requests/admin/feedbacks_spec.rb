385c55db58388f4b2fa95e634e5c32850f2c0e33
kae_kasui <kaekasui_github@icloud.com>
2016-02-08 14:48:31 +0900

    fixed for test

--- spec/requests/admin/feedbacks_spec.rb
+++ spec/requests/admin/feedbacks_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe 'GET /admin/feedbacks?offset=offset', autodoc: true do
+  include ActionView::Helpers::TextHelper
+
   let!(:user) { create(:email_user, :registered) }
   let!(:admin_user) { create(:email_user, :admin_user, :registered) }
   let!(:feedback) { create(:feedback) }
@@ -23,6 +25,9 @@ describe 'GET /admin/feedbacks?offset=offset', autodoc: true do
   end
 
   context '管理ユーザーとしてログインしている場合' do
+    before do
+    end
+
     context '1ページ以内のフィードバック数の場合' do
       it '200が返り、フィードバック一覧が返ってくること' do
         # TODO: Twitterユーザーのフィードバックを表示する
@@ -36,7 +41,7 @@ describe 'GET /admin/feedbacks?offset=offset', autodoc: true do
               email: user_feedback.email,
               user_id: user_feedback.user_id,
               user_name: user_feedback.user.try(:_name),
-              content: user_feedback.content,
+              content: simple_format(user_feedback.content),
               created_at: I18n.l(user_feedback.created_at)
             },
             {
@@ -44,7 +49,7 @@ describe 'GET /admin/feedbacks?offset=offset', autodoc: true do
               email: feedback.email,
               user_id: feedback.user_id,
               user_name: feedback.user.try(:_name),
-              content: feedback.content,
+              content: simple_format(feedback.content),
               created_at: I18n.l(feedback.created_at)
             }
           ],
@@ -66,7 +71,7 @@ describe 'GET /admin/feedbacks?offset=offset', autodoc: true do
               email: feedback.email,
               user_id: feedback.user_id,
               user_name: feedback.user.try(:_name),
-              content: feedback.content,
+              content: simple_format(feedback.content),
               created_at: I18n.l(feedback.created_at)
             }
           ],
