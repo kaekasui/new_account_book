79f853db27f47126060b146b68457860ffa2aa8f
kae_kasui <kaekasui_github@icloud.com>
2016-02-11 22:58:50 +0900

    set the icon padding

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :category do
     user
+    barance_of_payments { true | false }
     sequence(:name) { |i| "カテゴリ名#{i}" }
     sequence(:position) { |i| i - 1 }
   end
