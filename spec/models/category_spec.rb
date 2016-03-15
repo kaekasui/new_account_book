10741ed57a56de616c765ce1eb3624d93d2a5eb1
kae_kasui <kaekasui_github@icloud.com>
2016-03-15 16:32:19 +0900

    add the relationship for records

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Category, type: :model do
   it { is_expected.to have_many(:breakdowns) }
   it { is_expected.to have_many(:categorize_places) }
   it { is_expected.to have_many(:places).through(:categorize_places) }
+  it { is_expected.to have_many(:records) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_most(100) }
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Place, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to have_many(:categorize_places) }
   it { is_expected.to have_many(:categories).through(:categorize_places) }
+  it { is_expected.to have_many(:records) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_most(100) }
 end
