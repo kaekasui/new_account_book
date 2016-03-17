b5e0239cfd7dd51ba05742c3cc0b14758fdbb8d5
kae_kasui <kaekasui_github@icloud.com>
2016-03-17 17:16:24 +0900

    by rubocop

--- app/models/record.rb
+++ app/models/record.rb
@@ -5,6 +5,9 @@ class Record < ActiveRecord::Base
   belongs_to :place
 
   validates :published_at, presence: true
-  validates :charge, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :charge, presence: true,
+                     numericality: { only_integer: true,
+                                     greater_than_or_equal_to: 0,
+                                     allow_blank: true }
   validates :category, presence: true
 end
