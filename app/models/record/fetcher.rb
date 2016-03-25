0af3a20f6af7a21f266d248b3dadbb68577098ff
kae_kasui <kaekasui_github@icloud.com>
2016-03-25 17:23:26 +0900

    return the records on mypage

--- app/models/record/fetcher.rb
+++ app/models/record/fetcher.rb
@@ -1,13 +1,14 @@
 class Record::Fetcher
   attr_accessor :total_count
 
-  def initialize(user: nil, params: nil)
+  def initialize(user: nil, params: nil, sort_type: nil)
     @user = user
     @params = params
+    @sort_type = sort_type
   end
 
   def all
-    records = @user.records.order_type(@params[:sort])
+    records = @user.records.order_type(@sort_type)
     records = records.the_day(@params[:date]) if @params[:date].present?
     if @params[:year].present? || @params[:month].present?
       records = records.the_year_and_month(@params[:year], @params[:month])
--- app/views/mypages/show.json.jbuilder
+++ app/views/mypages/show.json.jbuilder
@@ -15,3 +15,15 @@ json.messages do
     json.created_at I18n.l(message.created_at)
   end
 end
+
+json.records do
+  json.array! @records do |record|
+    json.id record.id
+    json.published_at record.published_at
+    json.charge record.charge
+    json.category_name record.category.name
+    json.breakdown_name record.breakdown.try(:name)
+    json.place_name record.place.try(:name)
+    json.memo record.memo
+  end
+end
