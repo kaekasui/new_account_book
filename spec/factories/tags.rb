397d8ab0e675e34a6eb4c27481e42a6cfbcd5f35
kae_kasui <kaekasui_github@icloud.com>
2016-04-07 11:04:41 +0900

    add the color code

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :tag do
     user
     sequence(:name) { |i| "ラベル#{i}" }
-    color_code { format('%06x', (rand * 0xffffff)) }
+    color_code { '#' + format('%06x', (rand * 0xffffff)) }
   end
 end
