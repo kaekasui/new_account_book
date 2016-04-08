b20dfce95c80b2e2d95a380da381f5e65cba04cc
kae_kasui <kaekasui_github@icloud.com>
2016-04-08 16:31:36 +0900

    create and update tags, and a record has some tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,16 @@ class Tag < ActiveRecord::Base
 
   has_many :tagged_records
   has_many :records, through: :tagged_records
+
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: Settings.tag.name.maximum_length }
+
+  before_save :set_color_code
+
+  def set_color_code
+    hex_color = format('%06x', (rand * 0xfffff))
+    self.color_code = '#' + hex_color if color_code.blank?
+  end
 end
