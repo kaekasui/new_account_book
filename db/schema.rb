6664a90508d7a933743e81fe82562cd00f7cede1
kae_kasui <kaekasui_github@icloud.com>
2016-04-25 18:29:36 +0900

    bin/rails generate migration AddNewEmailToUsers new_email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414043428) do
+ActiveRecord::Schema.define(version: 20160425092850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -212,6 +212,7 @@ ActiveRecord::Schema.define(version: 20160414043428) do
     t.boolean  "place_field",            default: true
     t.boolean  "memo_field",             default: true
     t.boolean  "tag_field",              default: true
+    t.string   "new_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
