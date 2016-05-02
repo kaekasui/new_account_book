d67c11f296b222b1d2d339bbbbe34083d302fa33
kae_kasui <kaekasui_github@icloud.com>
2016-05-02 17:45:26 +0900

    fix a warning, 'Instead, please set the appropriate counter_cache options on the has_many and belongs_to for your associations to classifications'

--- app/models/categorize_place.rb
+++ app/models/categorize_place.rb
@@ -1,6 +1,6 @@
 class CategorizePlace < ActiveRecord::Base
   counter_culture :category, column_name: 'places_count'
-  belongs_to :category
+  belongs_to :category, counter_cache: 'places_count'
   belongs_to :place
 
   validates :category_id, uniqueness: { scope: :place_id }
