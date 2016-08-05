cc5a97d238c8c7e5136f878afb4636607c160660
kae_kasui <kaekasui_github@icloud.com>
2016-08-06 02:32:32 +0900

    modified by rubocop

--- app/models/capture.rb
+++ app/models/capture.rb
@@ -4,21 +4,19 @@ class Capture < ApplicationRecord
 
   attr_accessor :validate_check
 
-  with_options if: :validate_check do |cap|
-    cap.validates :published_at, presence: true
-    cap.validates :category_name,
-                  presence: true,
-                  length: { maximum: Settings.category.name.maximum_length }
-    cap.validates :breakdown_name,
-                  length: { maximum: Settings.breakdown.name.maximum_length }
-    cap.validates :place_name,
-                  length: { maximum: Settings.place.name.maximum_length }
-    cap.validates :charge,
-                  numericality: { only_integer: true,
-                                  greater_than_or_equal_to: 0,
-                                  less_than_or_equal_to: 9_999_999,
-                                  allow_blank: true }
-    cap.validates :memo,
-                  length: { maximum: Settings.record.memo.maximum_length }
-  end
+  validates :published_at, presence: true
+  validates :category_name,
+            presence: true,
+            length: { maximum: Settings.category.name.maximum_length }
+  validates :breakdown_name,
+            length: { maximum: Settings.breakdown.name.maximum_length }
+  validates :place_name,
+            length: { maximum: Settings.place.name.maximum_length }
+  validates :charge,
+            numericality: { only_integer: true,
+                            greater_than_or_equal_to: 0,
+                            less_than_or_equal_to: 9_999_999,
+                            allow_blank: true }
+  validates :memo,
+            length: { maximum: Settings.record.memo.maximum_length }
 end
